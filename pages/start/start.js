eac13c5d2f278f81a845ad95418a5215492e33ad
Alair <designeralair@126.com>
2020-02-28 17:57:23 +0800

    0.6.1

--- pages/start/start.js
+++ pages/start/start.js
@@ -7,6 +7,9 @@ Page({
     angle: 0,
     userInfo: {},
     moto: '设备配对中，请稍后！',
+    deviceId: '',
+    deviceName: "",
+    inputValue: '66:00:0A:0B:02:08', 
   },
  //事件处理函数，跳转主页 
   goToIndex: function() {
@@ -14,20 +17,232 @@ Page({
       url: '/pages/index/index',
     });
   },
-  onLoad: function(options) {
-    var that = this
-//    wx.setNavigationBarTitle({
-//      title: wx.getStorageSync('Airwater')
-//    })
 
-    if (options.q !== undefined) {
-      var scan_url = decodeURIComponent(options.q);
-      console.log(scan_url);
-      }else{
-        console.log(123);
+  onLoad: function(options) {
+    wx.showLoading({
+      title: '请打开蓝牙',
+    });
+    console.log(options.id);
+    this.setData({
+      deviceName: options.id,
+    });
+    console.log('设备的Name', this.data.deviceName);
+    var that = this;
+
+    var deviceName = options.id;
+    wx.closeBluetoothAdapter({
+      success: function(res) {
+        console.log('关闭蓝牙模块');
+        /* 初始化蓝牙适配器 */
+        wx.openBluetoothAdapter({
+          success: function(res) {
+            console.log('初始化蓝牙适配器成功');
+            wx.hideLoading();
+            wx.showLoading({
+              title: '请稍后....',
+            });
+            wx.startBluetoothDevicesDiscovery({
+              allowDuplicatesKey: false,
+              success: function(res) {
+                console.log('这里是开始搜索附近设备', res);
+                console.log('deviceName', deviceName);
+                //添加延迟
+                setTimeout(() => {
+                  wx.getBluetoothDevices({
+                    success: function(res) {
+                      console.log(res);
+                      //在搜索到的所有蓝牙中找到需要连接的那一个蓝牙
+                      for (var i = 0; i < res.devices.length; i++) {
+                        if (res.devices[i].name == deviceName) {
+                        //if (res.devices[i].name == that.data.inputValue) {
+                          that.setData({
+                            deviceId: res.devices[i].deviceId,
+                          })
+                          console.log(res.devices[i].deviceId)
+                          wx.hideLoading();
+                          /* 连接中动画 */
+                          wx.showLoading({
+                            title: '正在连接...',
+                          });
+                          that.CreateBLEConnection();
+                        }
+                      }
+                    },
+                    fail: function() {
+                      console.log("搜索蓝牙设备失败")
+                    }
+                  });
+                }, 2000);
+              },
+            });
+          },
+        })
       }
+    });
+  },
+  //连接蓝牙
+  CreateBLEConnection: function() {   
+    var that = this;
+    wx.stopBluetoothDevicesDiscovery({
+      success: function(res) {
+        console.log('停止搜索设备', res)
+      }
+    })
+    /* 开始连接蓝牙设备 */
+    wx.createBLEConnection({
+      deviceId: that.data.deviceId,
+      success: function(res) {
+        console.log('连接成功', res);
+        wx.hideLoading();
+        /* 获取设备的服务UUID */
+        wx.getBLEDeviceServices({
+          deviceId: that.data.deviceId,
+          success: function(service) {
+            var all_UUID = service.services; //取出所有的服务
+            console.log('所有的服务', all_UUID);
+            var UUID_lenght = all_UUID.length; //获取到服务数组的长度
+            /* 遍历服务数组 */
+            for (var index = 0; index < UUID_lenght; index++) {
+              var ergodic_UUID = all_UUID[index].uuid; //取出服务里面的UUID
+              var UUID_slice = ergodic_UUID.slice(4, 8); //截取4到8位
+              /* 判断是否是我们需要的FFE0 */
+              if (UUID_slice == 'FFE0' || UUID_slice == 'ffe0') {
+                var index_uuid = index;
+                that.setData({
+                  serviceId: all_UUID[index_uuid].uuid //确定需要的服务UUID
+                });
+              };
+            };
+            console.log('需要的服务UUID', that.data.serviceId)
+            that.Characteristics(); //调用获取特征值函数
+          },
+        });
+      },
+    })
+  },
+  Characteristics: function() {
+    var that = this;
+    var device_characteristics = [];
+    var characteristics_uuid = {};
+    wx.getBLEDeviceCharacteristics({
+      deviceId: that.data.deviceId,
+      serviceId: that.data.serviceId,
+      success: function(res) {
+        var characteristics = res.characteristics; //获取到所有特征值
+        var characteristics_length = characteristics.length; //获取到特征值数组的长度
+        console.log('获取到特征值', characteristics);
+        console.log('获取到特征值数组长度', characteristics_length);
+
+        /* 遍历获取characteristicsId */
+        for (var index = 0; index < characteristics_length; index++) {
+          var characteristics_UUID = characteristics[index].uuid; //取出特征值里面的UUID
+          var characteristics_slice = characteristics_UUID.slice(4, 8); //截取4到8位
+          /* 判断是否是我们需要的FFE1 */
+          if (characteristics_slice == 'FFE1' || characteristics_slice == 'ffe1') {
+            var index_uuid = index;
+            that.setData({
+              characteristicsId: characteristics[index_uuid].uuid //确定的写入UUID
+            });
+          };
+        };
+        console.log('写入characteristicsId', that.data.characteristicsId);
+        that.SendTap(); //发送指令
+      },
+    })
+  },
+  /* 发送指令 */
+  SendTap: function() {
+    var that = this;
+    var value_ascii = "";
+    var value_initial = "01"; //发送的信息
+    console.log('输入框中的值', value_initial);
+    /* 以Ascii字符发送 */
+    var value_split = value_initial.split(''); //将字符一个一个分开
+    console.log('value_split', value_split);
+    for (var i = 0; i < value_split.length; i++) {
+      value_ascii = value_ascii + value_split[i].charCodeAt().toString(16); //转为Ascii字符后连接起
+    }
+    var value = value_ascii;
+    console.log('转为Ascii码值', value);
+    var write_function = that.write(value); //调用数据发送函数
+  },
+  write: function(str) {
+    var that = this;
+    var value = str;
+    console.log('value', value);
+    /* 将数值转为ArrayBuffer类型数据 */
+    var typedArray = new Uint8Array(value.match(/[\da-f]{2}/gi).map(function(h) {
+      return parseInt(h, 16)
+    }));
+    var buffer = typedArray.buffer;
+    wx.writeBLECharacteristicValue({
+      deviceId: that.data.deviceId,
+      serviceId: that.data.serviceId,
+      characteristicId: that.data.characteristicsId,
+      value: buffer,
+      success: function(res) {
+        console.log('数据发送成功', res);
+        wx.showToast({
+          title: '发送成功',
+          icon: 'success',
+          duration: 2000
+        })
+      },
+      fail: function(res) {
+        console.log('调用失败', res);
+        /* 调用失败时，再次调用 */
+        wx.writeBLECharacteristicValue({
+          deviceId: that.data.deviceId,
+          serviceId: that.data.serviceId,
+          characteristicId: that.data.characteristicsId,
+          value: buffer,
+          success: function(res) {
+            console.log('第2次数据发送成功', res);
+            wx.showToast({
+              title: '发送成功',
+              icon: 'success',
+              duration: 2000
+            })
+          },
+          fail: function(res) {
+            console.log('第2次调用失败', res);
+            /* 调用失败时，再次调用 */
+            wx.writeBLECharacteristicValue({
+              deviceId: that.data.deviceId,
+              serviceId: that.data.serviceId,
+              characteristicId: that.data.characteristicsId,
+              value: buffer,
+              success: function(res) {
+                console.log('第3次数据发送成功', res);
+                wx.showToast({
+                  title: '发送成功',
+                  icon: 'success',
+                  duration: 2000
+                })
+              },
+              fail: function(res) {
+                console.log('第3次调用失败', res);
+              }
+            });
+          }
+        });
+      }
+    });
   },
 
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+    var that = this;
+    wx.closeBLEConnection({
+      deviceId: that.data.deviceId,
+      success: function(res) {
+        console.log('断开设备连接', res);
+      }
+    });
+  },
+/*****************启动界面***************** */
   onReady: function() {
     var that = this;
     setTimeout(function() {
@@ -49,5 +264,5 @@ Page({
         });
       }
     });
-  }
-});
+  },
+})
